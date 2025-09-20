ceb88e8a12767bb3761dab00a6d360ce828dc50d
musaabbas989-ctrl <musaabbas989@gmail.com>
2025-09-20 20:36:24 +0500

    Lateset code update

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet
+from .views import OrderViewSet, login_view
 
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
+    path("login/", login_view, name="login"),
     path('', include(router.urls)),
 ]
